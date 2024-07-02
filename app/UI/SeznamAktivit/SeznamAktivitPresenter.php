f63693e71ef04051919dfcb63c44abe85e37cf89
bob <matej.cernik@outlook.com>
2024-07-02 09:45:10 +0200

    přidávání vzdělávacích aktivit

---
+++ app/UI/SeznamAktivit/SeznamAktivitPresenter.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\UI\SeznamAktivit;
+use Nette\Application\UI\Form;
+
+use Nette;
+
+
+final class SeznamAktivitPresenter extends Nette\Application\UI\Presenter
+{
+    public function __construct(private Nette\Database\Explorer $database) 
+	{
+		$this->explorer = $database;
+	}
+
+	protected $explorer;
+
+	public function renderDefault(int $svpID): void
+	{
+		$plan = $this->explorer->table('svp')->get($svpID);
+		$this->template->jmenoSVP = $plan->jmenoSVP;
+
+		$this->template->svpID = $svpID;
+
+		//načtení vzdělávacích aktivit
+		$aktivity = $this->explorer->table('vzdelavaciAktivita')->where('svp_svpID = ?', $svpID)->fetchAll();
+		$this->template->aktivity = $aktivity;
+	}
+
+	protected function createComponentActivityForm(): Form
+	{
+		$form = new Form; // means Nette\Application\UI\Form
+
+		$form->addText('jmenoAktivity', 'Jméno vzdělvávací aktivity:')
+			->setRequired();
+
+		$form->addTextarea('popisAktivity', 'Popis vzdělvávací aktivity:');
+
+		$form->addSelect('typAktivity', 'Typ aktivity:', $this->explorer->table('typAktivity')->fetchPairs('typAktivityID', 'jmenoTypu'))
+			->setPrompt('Vyberte typ aktivity')
+			->setRequired();
+
+		$form->addSubmit('send', 'Přidat vzdělávací aktivitu');
+
+		$form->onSuccess[] = $this->activityFormSucceeded(...);
+
+		return $form;
+	}
+
+	private function activityFormSucceeded(\stdClass $data): void
+	{
+		$svpID = $this->getParameter('svpID');
+
+		$this->database->table('vzdelavaciAktivita')->insert([
+			'svp_svpID' => $svpID,
+			'jmenoAktivity' => $data->jmenoAktivity,
+			'popisAktivity' => $data->popisAktivity,
+			'typAktivity_typAktivityID' => $data->typAktivity
+		]);
+
+		$this->flashMessage('Vzdělávací obor úspěšně přidán', 'success');
+		$this->redirect('this');
+	}
+}
