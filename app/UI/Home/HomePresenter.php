49dc7dd1aad5e35236136ffc53adeae1b4aafa93
bob <matej.cernik@outlook.com>
2024-12-30 13:43:30 +0100

    kód pro vygenerování ročníků 8letého gymnázia

--- app/UI/Home/HomePresenter.php
+++ app/UI/Home/HomePresenter.php
@@ -120,6 +120,25 @@ final class HomePresenter extends Nette\Application\UI\Presenter
 		$this->redirect('this');
 	}
 
+	public function handleCreateGrades(): void
+	{
+		$grades = [
+			['nazevRocniku' => 'prima', 'popisRocniku' => '6. ročník osmiletého gymnázia - 6. ročník základní školy'],
+			['nazevRocniku' => 'sekunda', 'popisRocniku' => '7. ročník osmiletého gymnázia - 7. ročník základní školy'],
+			['nazevRocniku' => 'tercie', 'popisRocniku' => '8. ročník osmiletého gymnázia - 8. ročník základní školy'],
+			['nazevRocniku' => 'kvarta', 'popisRocniku' => '9. ročník osmiletého gymnázia - 9. ročník základní školy'],
+			['nazevRocniku' => 'kvinta', 'popisRocniku' => '1. ročník osmiletého gymnázia - 1. ročník střední školy'],
+			['nazevRocniku' => 'sexta', 'popisRocniku' => '2. ročník osmiletého gymnázia - 2. ročník střední školy'],
+			['nazevRocniku' => 'septima', 'popisRocniku' => '3. ročník osmiletého gymnázia - 3. ročník střední školy'],
+			['nazevRocniku' => 'oktáva', 'popisRocniku' => '4. ročník osmiletého gymnázia - 4. ročník střední školy']
+		];
+	
+		$this->database->table('rocnik')->insert($grades);
+	
+		$this->flashMessage('Ročníky úspěšně vytvořeny', 'success');
+		$this->redirect('this');
+	}
+
 	public function handleDeleteGrade(int $id): void
 	{
 		$this->database->table('rocnik')->where('rocnikID', $id)->delete();
